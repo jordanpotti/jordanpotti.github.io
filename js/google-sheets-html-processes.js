8f391d0cf237f377f1abc0246f7e80490e944cd3
JP <19959240+jordanpotti@users.noreply.github.com>
2020-05-13 00:12:58 -0600

    Update google-sheets-html-processes.js

--- js/google-sheets-html-processes.js
+++ js/google-sheets-html-processes.js
@@ -14,13 +14,13 @@ google.load('visualization', '1', {
 });
 var processes_visualization;
 
-function drawVisualization() {
+function drawVisualizationProcesses() {
     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=15nU4tmxYX-6_pDI79uxGEG9wCyB2fNeA4ALF4oG7jGA&gid=967627017&output=html&usp=sharing');
     query.setQuery('SELECT A, B, C, D');
-    query.send(handleQueryResponse);
+    query.send(handleQueryResponseProcessses);
 }
 
-function handleQueryResponse(response) {
+function handleQueryResponseProcessses(response) {
     if (response.isError()) {
         alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
         return;
@@ -32,4 +32,4 @@ function handleQueryResponse(response) {
         legend: 'bottom'
     });
 }
-google.setOnLoadCallback(drawVisualization);
+google.setOnLoadCallback(drawVisualizationProcesses);
